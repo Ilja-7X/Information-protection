75fdef20a56439eb49a01bfcd3049855b4360543
Ilya Oshlakov <oshlakov2014@gmail.com>
2022-11-07 21:39:05 +0700

    Refactor the code

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 
-import algorithms.cryptosys_public_key.PowFast;
 import algorithms.encryption.Shamir;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 
@@ -12,25 +9,19 @@ public class Main {
     public static void main(String[] args)
     {
         long P = Shamir.generatePublicP();
-        System.out.println("Random Integer value : " + P);
+        System.out.println("Random Integer: " + P);
         Shamir A = new Shamir(P);
         Shamir B = new Shamir(P);
-        /*byte[] buffer = Shamir.getBytesFromFile("Example/image.png");
-        Shamir.getFileFromBytes("Example/10.png", buffer);
-
-        System.out.println();*/
 
         try {
             List<Long> x1 = A.ShamirCalcIteration("Example/img.png", null, 1);
             List<Long> x2 = B.ShamirCalcIteration(null, x1, 2);
             List<Long> x3 = A.ShamirCalcIteration(null, x2, 3);
-            List<Long> x4 = B.ShamirCalcIteration("Example/testImg.png", x3, 4);
+            B.ShamirCalcIteration("Example/testImg.png", x3, 4);
         }
         catch(IOException ex)
         {
             System.out.println(ex.getMessage());
         }
-
-        //System.out.print("p");
     }
 }
--- src/algorithms/cryptosys_public_key/PowFast.java
+++ src/algorithms/cryptosys_public_key/PowFast.java
@@ -3,14 +3,11 @@ package algorithms.cryptosys_public_key;
 public class PowFast {
     public static long calculate( long a, long x, long p ) {
         long y = 1;
-        /*if (a < 0) {
-            System.out.println("a < 0"); //Negative numbers and p=0 are not supported
-            return -2;
-        }*/ if(x < 0) {
-            System.out.println("x < 0");
+        if(x < 0) {
+            System.out.println("PowFast: Negative numbers X");
             return -2;
         }else if (p <= 0) {
-            System.out.println("p <= 0");
+            System.out.println("PowFast: Negative numbers P");
             return -2;
         }
         else {
