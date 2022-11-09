1ed295f7d74f25c3eb3b0fbb230a1d12d50479d1
Ilya Oshlakov <oshlakov2014@gmail.com>
2022-11-10 00:22:08 +0700

    Add sending and receiving functions to the class Elgamal

--- src/algorithms/encryption/Elgamal.java
+++ src/algorithms/encryption/Elgamal.java
@@ -2,26 +2,32 @@ package algorithms.encryption;
 
 import algorithms.cryptosys_public_key.DiffieHellman;
 import algorithms.cryptosys_public_key.PowFast;
-import algorithms.encryption.*;
 
 import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Elgamal {
-    private long P;
-    private long G;
+    private static long P; //public parameter P
+    private static long G; //public parameter G
 
-    public Elgamal()
+    private long C; //private number of subscriber B
+    private long D; //public number of subscriber B
+    private long R;
+
+    public static void setInitialValues()
     {
         DiffieHellman Df = new DiffieHellman();
         P = Df.getP();
         G = Df.getG();
     }
 
-    public void calculateReceiveMassage()
+    public Elgamal()
     {
-
+        SecureRandom sr = new SecureRandom();
+        sr.setSeed(System.currentTimeMillis());
+        C = sr.nextInt((int)P - 2) + 1;
+        D = PowFast.calculate(G, C, P);
     }
 
     public List<Long> calculateSendMassage(String path, long publicKeyD)
@@ -30,25 +36,41 @@ public class Elgamal {
 
         SecureRandom sr = new SecureRandom();
         sr.setSeed(System.currentTimeMillis());
-        long K = sr.nextInt(1000000000 - 2) + 1;
+        long K = sr.nextInt((int)P - 2 - 1 ) + 1;
+        R = PowFast.calculate(G,K,P);
+
+        long intermediateValue = PowFast.calculate(publicKeyD, K, P);
+
 
         byte[] byteArray = Shamir.getBytesFromFile(path);
-        long R = PowFast.calculate(G,K,P);
 
         for (byte a : byteArray) {
-            //System.out.print(a + " ");
-            E.add(PowFast.calculate(a, publicKeyD, P));
+            E.add(PowFast.calculate(a * intermediateValue, 1, P));
         }
         return E;
-
     }
+    public void calculateReceiveMassage(long R, List<Long> E, String path)
+    {
+        byte[] buffer = new byte[E.size()];
+        long intermediateValue = PowFast.calculate(R,P - 1 - C, P);
+
+        for (int i = 0; i < E.size(); i++) {
+            buffer[i] = (byte) PowFast.calculate(E.get(i) * intermediateValue, 1, getP());
+        }
+        Shamir.getFileFromBytes(path,buffer);
+    }
+
+
     public long getP()
     {
         return P;
     }
-
-    public long getG()
+    public long getD()
     {
-        return G;
+        return D;
+    }
+    public long getR()
+    {
+        return R;
     }
 }
