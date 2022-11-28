51732ce2acd68133153e0d3fc916abefda812cda
Ilya Oshlakov <oshlakov2014@gmail.com>
2022-11-28 13:27:14 +0700

    Finish writing RSADigitalSign class

--- src/algorithms/digital_signature/RSADigitalSign.java
+++ src/algorithms/digital_signature/RSADigitalSign.java
@@ -2,18 +2,12 @@ package algorithms.digital_signature;
 
 import algorithms.cryptosys_public_key.PowFast;
 import algorithms.encryption.RSA;
-import algorithms.encryption.Shamir;
-import functionalfiles.FileBytes;
-import algorithms.cryptosys_public_key.*;
 
 import org.apache.commons.codec.digest.DigestUtils;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.io.FileInputStream;
+import java.io.*;
+import java.io.DataOutputStream;
 import java.math.BigInteger;
-
 
 public class RSADigitalSign {
     private long C;
@@ -25,14 +19,48 @@ public class RSADigitalSign {
         D = subscriber.getD();
         N = subscriber.getN();
     }
-    public void signFileMD5(String pathExistingFile, String pathNewFile) throws IOException {
-        String checksum = DigestUtils.md5Hex(new FileInputStream(pathExistingFile));
+    public void signFileMD5(String pathExistingFile, String pathDirectory) throws IOException {
+        File file = new File(pathExistingFile);
+        String nameFile = file.getName();
+        String newPath = pathDirectory + "/" + "checkKey-"+ nameFile + ".txt";
+        String checksum = DigestUtils.md5Hex(new FileInputStream(file));
         BigInteger hash = new BigInteger(checksum, 16);
         hash = hash.mod(new BigInteger(String.valueOf(N)));
-        List<Long> E = new ArrayList<>();
 
         long S = PowFast.calculate(hash.longValue(), C, N);
+        writeToFile(newPath, S);
     }
+    public void checkSign(String pathFile, String pathSignFile, long D, long N) throws IOException {
+        String checksum = DigestUtils.md5Hex(new FileInputStream(pathFile));
+        BigInteger hash = new BigInteger(checksum, 16);
+        hash = hash.mod(new BigInteger(String.valueOf(N)));
+        System.out.println("Hash = " + hash.longValue());
+
+
+        long S = readToFile(pathSignFile);
+
+        long w = PowFast.calculate(S, D, N);
+        System.out.println("Сalculated value = " + w);
+
+        if(w != hash.longValue()){
+            System.out.println("Note: digital signature RSA is invalid");
+        }
+        else
+            System.out.println("Note: digital signature RSA is valid");
+    }
+
+    public static void writeToFile(String path, long numWrite) throws IOException {
+        DataOutputStream dos = new DataOutputStream(new FileOutputStream(path));
+        dos.writeLong(numWrite);
+    }
+
+    public static long readToFile(String path) throws IOException {
+        DataInputStream inputStream = new DataInputStream(new FileInputStream(path));
+        return inputStream.readLong();
+    }
+
+
+
     public long getN() {
         return N;
     }
--- src/main/digitalsign/MainRSADigitalSign.java
+++ src/main/digitalsign/MainRSADigitalSign.java
@@ -6,7 +6,13 @@ import java.io.IOException;
 
 public class MainRSADigitalSign {
     public static void main(String[] args) {
-        RSADigitalSign A = new RSADigitalSign();
-        RSADigitalSign B = new RSADigitalSign();
+        try {
+            RSADigitalSign A = new RSADigitalSign();
+            A.signFileMD5("Example/img.png", "Result/digitalsign/RSA");
+            RSADigitalSign B = new RSADigitalSign();
+            B.checkSign("Example/img.png", "Result/digitalsign/RSA/checkKey-img.png.txt", A.getD(), A.getN());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
