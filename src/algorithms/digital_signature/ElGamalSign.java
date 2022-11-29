1aa83ee27f7160f40d8a48afd2cf78a991e74d39
Ilya Oshlakov <oshlakov2014@gmail.com>
2022-11-30 01:48:39 +0700

    Add a constructor and signfile function ElGamalSign class

--- src/algorithms/digital_signature/ElGamalSign.java
+++ src/algorithms/digital_signature/ElGamalSign.java
@@ -25,6 +25,40 @@ public class ElGamalSign {
         setG(diffieHellman.getG());
     }
 
+    public ElGamalSign() {
+        SecureRandom srand = new SecureRandom();
+        srand.setSeed(System.currentTimeMillis());
+        System.out.println("P" + P);
+        System.out.println("G" + G);
+        X = srand.nextInt(((int)P-2)+1);
+        Y = PowFast.calculate(G, X, P);
+    }
+
+    public void signFile(String pathFile) throws IOException {
+        long k, k_1;
+        SecureRandom srand = new SecureRandom();
+        srand.setSeed(System.currentTimeMillis());
+
+        String checksumMD5 = DigestUtils.md5Hex(new FileInputStream(pathFile));
+        BigInteger hash = new BigInteger(checksumMD5, 16);
+        hash = hash.mod(new BigInteger(String.valueOf(P)));
+
+        long[] EuclidResult;
+        do {
+            do {
+                k = srand.nextInt((int)P-3)+2;
+                EuclidResult =  Euclid.calculate(k, P-1);
+            } while(EuclidResult[0] != 1);
+            k_1 = EuclidResult[2] + (P-1);
+        } while(k*k_1%(P-1) != 1);
+
+        r = PowFast.calculate(G, k, P);
+        System.out.println("r = " + r);
+        BigInteger u = hash.subtract(BigInteger.valueOf(X*r)).mod(BigInteger.valueOf(P-1));
+        S = u.multiply(BigInteger.valueOf(k_1)).mod(BigInteger.valueOf(P-1));
+        System.out.println("S = " + S);
+    }
+
     public static void setP(long p) {
         P = p;
     }
